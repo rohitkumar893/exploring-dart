693c89452f7ff7869db189a81054d1cfa9ac6c03
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-07-22 12:07:13 +0530

    dart inheritance

--- exploring dart.dart
+++ exploring dart.dart
@@ -5,8 +5,20 @@ void main(){
     args(1,2);
 }
 
-//Passing arguments:
+//Passing arguments in dart:
 void args(x,y){
     print(x+y);
 }
 
+//Inheritance in dart
+class Animal {
+  void makeSound() {
+    print('Some generic animal sound');
+  }
+}
+
+class Dog extends Animal {
+  void bark() {
+    print('Woof!');
+  }
+}
